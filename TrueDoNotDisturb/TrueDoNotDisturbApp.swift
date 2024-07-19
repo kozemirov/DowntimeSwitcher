d4dec0b03d6c131105366151878358ff768bef3f
Pavel Kozemirov <pskozemirov@gmail.com>
2024-07-19 22:39:39 +0500

    Add menu

--- TrueDoNotDisturb/TrueDoNotDisturbApp.swift
+++ TrueDoNotDisturb/TrueDoNotDisturbApp.swift
@@ -38,6 +38,8 @@ class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
         self.popover.contentSize = NSSize(width: 300, height: 300)
         self.popover.behavior = .transient
         self.popover.contentViewController = NSHostingController(rootView: ContentView(vm: self.downtimeVM))
+        
+        setupMenus()
     }
     
     @objc func togglePopover() {
@@ -52,5 +54,46 @@ class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
                 popover.show(relativeTo: button.bounds, of:button, preferredEdge: NSRectEdge.minY)
             }
         }
+    }
+    
+    func setupMenus() {
+        // 1
+        let menu = NSMenu()
+
+        // 2
+        let one = NSMenuItem(title: "One", action: #selector(didTapOne) , keyEquivalent: "1")
+        menu.addItem(one)
+
+        let two = NSMenuItem(title: "Two", action: #selector(didTapTwo) , keyEquivalent: "2")
+        menu.addItem(two)
+
+        let three = NSMenuItem(title: "Three", action: #selector(didTapThree) , keyEquivalent: "3")
+        menu.addItem(three)
+
+        menu.addItem(NSMenuItem.separator())
+
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+
+        // 3
+        statusItem.menu = menu
+    }
+    
+    // 1
+    private func changeStatusBarButton(number: Int) {
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "\(number).circle", accessibilityDescription: number.description)
+        }
+    }
+
+    @objc func didTapOne() {
+        changeStatusBarButton(number: 1)
+    }
+
+    @objc func didTapTwo() {
+        changeStatusBarButton(number: 2)
+    }
+
+    @objc func didTapThree() {
+        changeStatusBarButton(number: 3)
     }
 }
